bd1ffd71d4493972bb0c7b03c1f1cab6d33a6091
CRSylar <cristianoromaldetti@icloud.com>
2023-03-28 00:11:50 +0200

    prettier broke the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ const Cell = ({value, block, elementId} : {value: number|null, block: number, el
     <input
       className="bg-transparent cursor-pointer caret-transparent focus:bg-teal-100 outline-none border border-teal-900/10 text-center w-20 h-20 text-5xl disabled:cursor-not-allowed"
       type="number"
-      value={value?? board[block][elementId] || undefined}
+      value={value?? board[block][elementId]?? undefined}
       disabled={!!value}
       onChange={saveValue}
       onKeyPress={sanitizeInput}
